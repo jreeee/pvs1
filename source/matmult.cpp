478fdb4d69d675a911cdf7e6d9d438e09a78e71e
Jeremias Kilian <jeremias.michael.kilian@uni-weimar.de>
2020-11-19 01:23:20 +0100

    added support for multithreading, a counter and a mode selector

--- source/matmult.cpp
+++ source/matmult.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <omp.h>
 
 // ---------------------------------------------------------------------------
 // allocate space for empty matrix A[row][col]
@@ -59,49 +60,69 @@ void free_mat(float **A, int num_rows) {
 int main(int argc, char *argv[])
 {
 	float **A, **B, **C;	// matrices
-    int d1, d2, d3;         // dimensions of matrices
-    int i, j, k;			// loop variables
+  int d1, d2, d3;       // dimensions of matrices
+  int i, j, k;			    // loop variables
+  double start, end;    //time variables
+  int mode = 0;         //mode of the program
 
-    /* print user instruction */
-    if (argc != 4)
-    {
-        printf ("Matrix multiplication: C = A x B\n");
-        printf ("Usage: %s <NumRowA> <NumColA> <NumColB>\n", argv[0]); 
-        return 0;
-    }
+  /* print user instruction */
+  if (argc < 4)
+  {
+      printf ("Matrix multiplication: C = A x B\n");
+      printf ("Usage: %s <NumRowA> <NumColA> <NumColB> |optional| <mode>\n", argv[0]); 
+      return 0;
+  }
 
-    /* read user input */
-    d1 = atoi(argv[1]);		// rows of A and C
-    d2 = atoi(argv[2]);     // cols of A and rows of B
-    d3 = atoi(argv[3]);     // cols of B and C
+  /* read user input */
+  d1 = atoi(argv[1]);		  // rows of A and C
+  d2 = atoi(argv[2]);     // cols of A and rows of B
+  d3 = atoi(argv[3]);     // cols of B and C
+  mode = atoi(argv[4]);   // mode of the program
 
-    printf("Matrix sizes C[%d][%d] = A[%d][%d] x B[%d][%d]\n", d1, d3, d1, d2, d2, d3);
+  if (mode < 0 || mode > 3) {
+    printf( "\nusage of mode:\n\n0 - single core, verbose\n1 - single core, silent\n");
+    printf( "2 - multi core, verbose\n3 - multi core, silent\n\ndefault value is 0\n");
+    mode = 0;
+  }
 
-    /* prepare matrices */
-    A = alloc_mat(d1, d2);
-    init_mat(A, d1, d2); 
-    B = alloc_mat(d2, d3);
-    init_mat(B, d2, d3);
-    C = alloc_mat(d1, d3);	// no initialisation of C, because it gets filled by matmult
+  printf("Matrix sizes C[%d][%d] = A[%d][%d] x B[%d][%d]\n", d1, d3, d1, d2, d2, d3);
 
-    /* serial version of matmult */
-    printf("Perform matrix multiplication...\n");
+  /* prepare matrices */
+  A = alloc_mat(d1, d2);
+  init_mat(A, d1, d2); 
+  B = alloc_mat(d2, d3);
+  init_mat(B, d2, d3);
+  C = alloc_mat(d1, d3);	// no initialisation of C, because it gets filled by matmult
+
+  /* serial version of matmult */
+  if (mode > 1) {
+    printf("Performing matrix multiplication using %d cores...\n", omp_get_max_threads());
+  } else {
+    printf("Performing matrix multiplication using 1 core...\n");
+  }
+  start = omp_get_wtime();
+  #pragma parallel for
+  { 
     for (i = 0; i < d1; i++)
-       for (j = 0; j < d3; j++)
+      for (j = 0; j < d3; j++)
           for (k = 0; k < d2; k++)
-             C[i][j] += A[i][k] * B[k][j];
-
-    /* test output */
+            C[i][j] += A[i][k] * B[k][j];
+  }
+  end = omp_get_wtime();
+  /* test output */
+  if (mode % 2 == 0) {
     print_mat(A, d1, d2, "A"); 
     print_mat(B, d2, d3, "B"); 
-    print_mat(C, d1, d3, "C"); 
+    print_mat(C, d1, d3, "C");
+  }
 
-    printf ("\nDone.\n");
-	
-    /* free dynamic memory */
-    free_mat(A, d1);
-    free_mat(B, d2);
-    free_mat(C, d1);	
+  printf("\nMatrix multiplication took %3.1f seconds\n", end - start);
+  printf ("\nDone.\n");
 
-    return 0;
+  /* free dynamic memory */
+  free_mat(A, d1);
+  free_mat(B, d2);
+  free_mat(C, d1);	
+
+  return 0;
 }
